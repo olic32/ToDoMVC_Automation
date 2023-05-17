279ad7031e993dff815e7559dd3b295b07ff696d
olic32 <olicullimore@hotmail.co.uk>
2023-05-17 17:25:20 +0100

    Ollie end of day commit - added Homepage POM and todo POM

--- app/src/test/java/todomvc_automation/ToDoMVCChromeTest.java
+++ app/src/test/java/todomvc_automation/ToDoMVCChromeTest.java
@@ -25,34 +25,141 @@ public class ToDoMVCChromeTest {
         driver = new ChromeDriver(options);
     }
 
+    @BeforeEach
+    public void launchBrowserSmall() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
     @Test
-    public void addEntryCheckTop() {
-
-        //Create instance of the homepage POM class
+    public void singleCharacterBackbone() {
         HomepagePOM homePageSelect = new HomepagePOM(driver);
-
-        //Navigate to the first framework
         homePageSelect.navigateToFramework("Backbone.js");
-
-        //Create generic instance of the framework page (same across frameworks)
         ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
-
-        //Then from heere you can run your tests on the first framework
-        //For example - add "Hello to the input field"
         frameworkPageSelect.inputIntoToDoField("a");
-
-        //Then press enter
         frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
 
-        //Check what is at the top of the list
-        String topOfList = frameworkPageSelect.checkTopOfList();
-
-        //Assert it's what we inputted
-        assertEquals(topOfList, "a");
-
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
     }
     @Test
-    public void checkMultipleEntries() {
+    public void singleCharacterKnockoutJS() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("KnockoutJS");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+    @Test
+    public void singleCharacterDojo() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("Dojo");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+    @Test
+    public void singleCharacterKnockback() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("Knockback.js");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+    @Test
+    public void singleCharacterCanJS() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("CanJS");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+    @Test
+    public void singleCharacterMithril() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("Mithril");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+    @Test
+    public void singleCharacterVuejs() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("Vue.js");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+    @Test
+    public void singleCharacterMarionettejs() {
+        HomepagePOM homePageSelect = new HomepagePOM(driver);
+        homePageSelect.navigateToFramework("Marionette.js");
+        ToDoPOM frameworkPageSelect = new ToDoPOM(driver);
+        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.enterInputTodo();
+        List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
+        List<String> expectedTodos = Arrays.asList("a");
+
+        for (WebElement todo : allTodos) {
+            String thisOne = todo.getText();
+            assertTrue(expectedTodos.contains(thisOne));
+        }
+    }
+
+
+
+
+
+
+    @Test
+    public void checkMultipleEntriesBackBone() {
 
         //Create instance of the homepage POM class
         HomepagePOM homePageSelect = new HomepagePOM(driver);
@@ -65,21 +172,26 @@ public class ToDoMVCChromeTest {
 
         //Then from heere you can run your tests on the first framework
         //For example - add "Hello to the input field"
-        frameworkPageSelect.inputIntoToDoField("a");
+        frameworkPageSelect.inputIntoToDoField("Hello");
         frameworkPageSelect.enterInputTodo();
-        frameworkPageSelect.inputIntoToDoField("ö");
+        frameworkPageSelect.inputIntoToDoField("Hello too");
         frameworkPageSelect.enterInputTodo();
-        frameworkPageSelect.inputIntoToDoField("c");
+        frameworkPageSelect.inputIntoToDoField("Hello three");
         frameworkPageSelect.enterInputTodo();
 
         List<WebElement> allTodos = frameworkPageSelect.getAllTodos();
-        List<String> expectedTodos = Arrays.asList("a", "ö", "c");
+        List<String> expectedTodos = Arrays.asList("Hello", "Hello too", "Hello three");
 
         for (WebElement todo : allTodos) {
             String thisOne = todo.getText();
             assertTrue(expectedTodos.contains(thisOne));
         }
 
+    }
+
+    @AfterEach
+    public void closeBrowserBetween() {
+        driver.quit();
     }
 
     @AfterAll
